e04d8d41d4b21f432a8933d1dafc9b72eb50812e
PartoTheEagle <asalhaytham@gmail.com>
2016-09-24 23:48:10 +0100

    Add GET/user/:email to users routes

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -99,4 +99,25 @@ router.patch('/:id', function (req, res, next) {
         });
     });
 });
+//GET user by eamil
+router.get('/:email', function (req, res, next) {
+   User.findOne({email: req.params.email}, function (err, user) {
+       if(err){
+           return res.status(500).json({
+               title: 'An error occurred',
+               error: err
+           });
+       }
+       if(!user) {
+           return res.status(404).json({
+               title: "An error occurred",
+               message: "user not found"
+           });
+       }
+       res.status(200).json({
+           message: 'success',
+           user: user
+       });
+   });
+});
 module.exports = router;
