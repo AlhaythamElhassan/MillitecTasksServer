536890919b623b3c8d8818fa79b15ab3b6f46fc9
PartoTheEagle <asalhaytham@gmail.com>
2016-09-25 18:52:52 +0100

    Adding routing and Authentication Component

--- assets/app/app.modules.ts
+++ assets/app/app.modules.ts
@@ -5,13 +5,21 @@ import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import {AppComponent} from "./app.component";
 import {HeaderComponent} from "./headerComponent/header.componet";
+import {LocationStrategy, HashLocationStrategy} from "@angular/common";
+import {AuthenticationComponent} from "./authenticationComponent/authenticaiton.component";
 
 @NgModule({
-    imports:      [ BrowserModule ],
+    imports:      [
+        BrowserModule
+    ],
     declarations: [
         AppComponent,
-        HeaderComponent
+        HeaderComponent,
+        AuthenticationComponent
     ],
+    providers: [
+        {provide: LocationStrategy, useClass: HashLocationStrategy}
+        ],
     bootstrap: [AppComponent]
 })
 export class AppModule {}
---
+++ assets/app/authenticationComponent/authenticaiton.component.ts
@@ -0,0 +1,10 @@
+import {Component} from "@angular/core";
+
+@Component({
+    moduleId: module.id,
+    selector: 'my-auth',
+    templateUrl: 'authentication.component.html',
+    styleUrls: ['authentication.component.css']
+})
+export class AuthenticationComponent {
+}
